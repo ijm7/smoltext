18d06edc00e238ff0995cd58a8343a809ce0aa93
ijm7 <ijmorton500@gmail.com>
2017-12-19 14:46:55 +0000

    Basic gui implemented

--- src/smoltext.erl
+++ src/smoltext.erl
@@ -3,7 +3,7 @@
 -include_lib("wx/include/wx.hrl").
  
 start() ->
-    State = make_window().
+    State = make_window(),
     loop(State),
     wx:destroy().
  
@@ -12,10 +12,17 @@ make_window() ->
     Frame = wxFrame:new(Server, -1, "smoltext"),
     wxTopLevelWindow:maximize(Frame),
     Panel = wxPanel:new(Frame),
-    Entry = wxTextCtrl:new(Panel, -1, [{style, ?wxTE_MULTILINE}]),
+    TextBox = wxTextCtrl:new(Panel, -1, [{style, ?wxTE_MULTILINE}]),
+    TextBoxSizer = wxBoxSizer:new(?wxHORIZONTAL),
+    wxSizer:add(TextBoxSizer, TextBox, []),
+    wxFrame:show(Frame),
+    wxFrame:connect(Frame, close_window),
+    wxPanel:connect(Panel, command_button_clicked),
+    {Frame, TextBox, self()}.
 
 loop(State) ->
-receive
+	{Frame, TextBox, Pid} = State,
+	receive
         #wx{event=#wxClose{}} ->	
 			if
 				Pid /= self() -> Pid ! { -1 };
@@ -25,9 +32,6 @@ receive
 			wxWindow:destroy(Frame),
 			ok;
         #wx{id = ?wxID_EXIT, event=#wxCommand{type = command_button_clicked} } ->
-			%%     {wx, ?wxID_EXIT, _,_,_} ->
-			%this message is sent when the exit button (ID 102) is clicked
-            %the other fields in the tuple are not important to us.
 			if
 				Pid /= self() -> Pid ! { -1 };
 				true -> ok
@@ -35,31 +39,7 @@ receive
             io:format("~p Closing window ~n",[self()]), %optional, goes to shell
 			wxWindow:destroy(Frame),
 			ok;  % we exit the loop
- 
-        #wx{id = 101, event=#wxCommand{type = command_button_clicked}} ->
-            %this message is sent when the Countdown button (ID 101) is clicked
-            T1001_val = wxTextCtrl:getValue(T1001),
-            case is_valid_list_to_integer(T1001_val) of
-                true ->
-                    T1001_int = list_to_integer(T1001_val),
-					MyPid = self(),
-					CntdwnPid = spawn( fun() -> cntdwn( T1001_int, MyPid ) end );
-                _ ->
-                    wxStaticText:setLabel(ST2001, "Only integers are allowed"),
-					CntdwnPid = Pid
-            end,
-            loop( {Frame, T1001, ST2001, CntdwnPid } );
-				{ 0 } ->
-					OutputStr = "ZERO",
-					wxStaticText:setLabel(ST2001, OutputStr),
-					loop( State );            
-                { N } ->
-					OutputStr = integer_to_list(N),
-					wxStaticText:setLabel(ST2001, OutputStr),
-					loop( State );
         Msg ->
-            %everything else ends up here
-            io:format("loop default triggered: Got ~n ~p ~n", [Msg]),
             loop(State)
  
     end.
