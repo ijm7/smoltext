bfb527e699b3ec3d12a0665d5d1d8c0c8ad4bb9d
ijm7 <ijmorton500@gmail.com>
2018-01-05 18:12:09 +0000

    modularise file menu functions

---
+++ src/menu_file.erl
@@ -0,0 +1,45 @@
+-module(menu_file).
+-compile(export_all).
+-include_lib("wx/include/wx.hrl").
+
+aboutDialog(Frame) ->
+	About = wxDialog:new(Frame, ?wxID_ABOUT, "About", [{style, ?wxDEFAULT_DIALOG_STYLE}]),
+	AboutText = "Smoltext - Version 0.1\n\nSmoltext is a basic text editor with simple functionality.\nTo report issues or suggest additions, please\ngo to https://github.com/ijm7/smoltext/issues\n\n",
+	AboutMessage = wxStaticText:new(About, -1, AboutText, [{style, ?wxALIGN_CENTRE}]),
+	OkButton = wxButton:new(About, ?wxID_OK, [{label, "OK"}]),
+	AboutSizer = wxBoxSizer:new(?wxVERTICAL),
+	{_, VerticalSize} = wxWindow:getSize(About),
+	AboutMessagePosition = trunc(VerticalSize/8),
+	wxSizer:addSpacer(AboutSizer, AboutMessagePosition),
+	wxSizer:add(AboutSizer, AboutMessage, [{flag, ?wxALIGN_CENTRE_HORIZONTAL}, {flag, ?wxALIGN_CENTRE_VERTICAL}]),
+	wxSizer:add(AboutSizer, OkButton, [{flag, ?wxALIGN_CENTRE_HORIZONTAL}, {flag, ?wxALIGN_CENTRE_VERTICAL}]),
+	wxDialog:setSizer(About, AboutSizer),
+	wxDialog:showModal(About),
+	ok.
+
+saveFile(TextBox, [H|_]) ->
+	wxStyledTextCtrl:saveFile(TextBox, H).
+
+saveAsFile(Frame, TextBox) ->
+	SaveFileDialog = wxFileDialog:new(Frame, [{style, ?wxFD_SAVE}]),
+	ReturnCode = wxDialog:showModal(SaveFileDialog),
+	if 
+		ReturnCode == ?wxID_OK ->
+			SavePath = wxFileDialog:getPath(SaveFileDialog),
+			wxStyledTextCtrl:saveFile(TextBox, SavePath),
+			SavePath;
+		true -> 
+			""
+	end.
+	
+openFile(Frame, TextBox) ->
+	OpenFileDialog = wxFileDialog:new(Frame, [{style, ?wxFD_OPEN}]),
+	ReturnCode = wxDialog:showModal(OpenFileDialog),
+	if 
+		ReturnCode == ?wxID_OK ->
+			LoadPath = wxFileDialog:getPath(OpenFileDialog),
+			wxStyledTextCtrl:loadFile(TextBox, LoadPath),
+			LoadPath;
+		true -> 
+			""
+	end.
