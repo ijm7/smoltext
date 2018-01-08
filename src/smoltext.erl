659f3fa4d962329a5e1d65389a11bf4ffbe17077
Iain Morton <ijmorton500@gmail.com>
2018-01-08 21:21:48 +0000

    Modular components (#8)
    * modularise file menu functions
    
    * Move close functionality to menu

--- src/smoltext.erl
+++ src/smoltext.erl
@@ -49,11 +49,11 @@ loop(State) ->
 	{Frame, _, TextBox, Files, Pid} = State,
 	receive
         #wx{event=#wxClose{}} ->	
-			closeWindow(Frame, Pid);
+			menu_file:closeWindow(Frame, Pid);
         #wx{id = ?wxID_EXIT, event=#wxCommand{type = command_menu_selected} } ->
-			closeWindow(Frame, Pid);
+			menu_file:closeWindow(Frame, Pid);
 		#wx{id = ?wxID_ABOUT, event= #wxCommand{type = command_menu_selected} } ->
-			aboutDialog(Frame),
+			menu_file:aboutDialog(Frame),
 			loop(State);
 		#wx{event=#wxStyledText{type = stc_updateui}} ->
 			updateStatusBar(Frame, TextBox),
@@ -62,21 +62,21 @@ loop(State) ->
 			spawn(fun() -> start() end),
 			loop(State);
 		#wx{id = 2, event=#wxCommand{type = command_menu_selected} } ->
-			FileName = openFile(Frame, TextBox),
+			FileName = menu_file:openFile(Frame, TextBox),
 			AddFile = lists:append([Files, [FileName]]),
 			updateTitle(Frame, AddFile),
 			loop({Frame, [], TextBox, AddFile, Pid});
 		#wx{id = 3, event=#wxCommand{type = command_menu_selected} } ->
 			if 
 				Files /= [] ->
-					saveFile(TextBox, Files),
+					menu_file:saveFile(TextBox, Files),
 					loop(State);
 				true ->
 					loop(State)
 				end;
 			
 		#wx{id = 4, event=#wxCommand{type = command_menu_selected} } ->
-			FileName = saveAsFile(Frame, TextBox),
+			FileName = menu_file:saveAsFile(Frame, TextBox),
 			AddFile = lists:append([Files, [FileName]]),
 			updateTitle(Frame, AddFile),
 			loop({Frame, [], TextBox, AddFile, Pid});
@@ -84,42 +84,6 @@ loop(State) ->
 			%io:fwrite("~w~n", [Msg]),
             loop(State)
     end.
-
-closeWindow(Frame, Pid) ->
-	if
-		Pid /= self() -> Pid ! { -1 };
-		true -> ok
-    end,
-    io:format("~p Closing window ~n",[self()]),
-	wxWindow:destroy(Frame),
-	ok.
-
-saveFile(TextBox, [H|_]) ->
-	wxStyledTextCtrl:saveFile(TextBox, H).
-
-saveAsFile(Frame, TextBox) ->
-	SaveFileDialog = wxFileDialog:new(Frame, [{style, ?wxFD_SAVE}]),
-	ReturnCode = wxDialog:showModal(SaveFileDialog),
-	if 
-		ReturnCode == ?wxID_OK ->
-			SavePath = wxFileDialog:getPath(SaveFileDialog),
-			wxStyledTextCtrl:saveFile(TextBox, SavePath),
-			SavePath;
-		true -> 
-			""
-	end.
-	
-openFile(Frame, TextBox) ->
-	OpenFileDialog = wxFileDialog:new(Frame, [{style, ?wxFD_OPEN}]),
-	ReturnCode = wxDialog:showModal(OpenFileDialog),
-	if 
-		ReturnCode == ?wxID_OK ->
-			LoadPath = wxFileDialog:getPath(OpenFileDialog),
-			wxStyledTextCtrl:loadFile(TextBox, LoadPath),
-			LoadPath;
-		true -> 
-			""
-	end.
 
 updateTitle(Frame, [H|_]) ->
 	wxWindow:setLabel(Frame, "smoltext - " ++ filename:basename(H)),
@@ -132,17 +96,3 @@ updateStatusBar(Frame, TextBox) ->
 	wxFrame:setStatusText(Frame, StatusText),
 	ok.
 	
-aboutDialog(Frame) ->
-	About = wxDialog:new(Frame, ?wxID_ABOUT, "About", [{style, ?wxDEFAULT_DIALOG_STYLE}]),
-	AboutText = "Smoltext - Version 0.1\n\nSmoltext is a basic text editor with simple functionality.\nTo report issues or suggest additions, please\ngo to https://github.com/ijm7/smoltext/issues\n\n",
-	AboutMessage = wxStaticText:new(About, -1, AboutText, [{style, ?wxALIGN_CENTRE}]),
-	OkButton = wxButton:new(About, ?wxID_OK, [{label, "OK"}]),
-	AboutSizer = wxBoxSizer:new(?wxVERTICAL),
-	{_, VerticalSize} = wxWindow:getSize(About),
-	AboutMessagePosition = trunc(VerticalSize/8),
-	wxSizer:addSpacer(AboutSizer, AboutMessagePosition),
-	wxSizer:add(AboutSizer, AboutMessage, [{flag, ?wxALIGN_CENTRE_HORIZONTAL}, {flag, ?wxALIGN_CENTRE_VERTICAL}]),
-	wxSizer:add(AboutSizer, OkButton, [{flag, ?wxALIGN_CENTRE_HORIZONTAL}, {flag, ?wxALIGN_CENTRE_VERTICAL}]),
-	wxDialog:setSizer(About, AboutSizer),
-	wxDialog:showModal(About),
-	ok.
